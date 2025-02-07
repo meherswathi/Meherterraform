ac3962f8f137bfc40ce3ae3afb71b2c7a74de0a8
Meher Swathi Avunoori <144318821+meherswathi@users.noreply.github.com>
2025-02-07 08:52:24 +0530

    Update main.tf

--- Day1-Ec2Creation/main.tf
+++ Day1-Ec2Creation/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "Local" {
     ami= "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     key_name = "Developer2"
-    security_groups = [ default ]
+    security_groups = ['default']
   
 }
